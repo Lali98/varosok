a7de1328fbb49367ea9bfe9ae2a5b0fa964d0df0
Lali98 <kisslajosbertalan77@gmail.com>
2022-05-31 14:10:14 +0200

    Kész

--- index.php
+++ index.php
@@ -14,34 +14,38 @@ $varos = new Varosok();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Magyarország</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="alap.css">
 </head>
 <body>
     <h1>Magyarország</h1>
-    <div>
-        <h2>Megyék</h2>
-        <ul>
-            <?php
-            foreach ($megye->allMegye() as $key=>$item)
-            {
-            ?>
-                <li onclick="location.href='index.php?id=<?= $item['id'] ?>'"><?= $item['mnev'] ?></li>
-            <?php
-            }
-            ?>
-        </ul>
-    </div>
-    <div id="varos">
-        <h2>Városok</h2>
-        <ul>
-            <?php
-            foreach ($varos->getVaros(intval($_GET['id'])) as $key=>$item)
-            {
-            ?>
-                <li onclick="location.href='varos.php?vid=<?= $item['id'] ?>'"><?= $item['vnev'] ?></li>
-            <?php
-            }
-            ?>
-        </ul>
+    <div id="egyut">
+        <div id="megye">
+            <h2>Megyék</h2>
+            <ul>
+                <?php
+                foreach ($megye->allMegye() as $key=>$item)
+                {
+                ?>
+                    <li onclick="location.href='index.php?id=<?= $item['id'] ?>'"><?= $item['mnev'] ?></li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
+        <div id="varos">
+            <h2>Városok</h2>
+            <ul>
+                <?php
+                foreach ($varos->getVaros(intval($_GET['id'])) as $key=>$item)
+                {
+                ?>
+                    <li onclick="location.href='varos.php?vid=<?= $item['id'] ?>'"><?= $item['vnev'] ?></li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
     </div>
 </body>
 </html>
--- varos.php
+++ varos.php
@@ -12,8 +12,18 @@ $varosok = new Varosok();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="alap.css">
+    <link rel="stylesheet" href="varos.css">
 </head>
 <body>
     <h1><?= $varosok->getVarosInfo(intval($_GET['vid']))['vnev'] ?></h1>
+    <ul>
+        <li>Megye: <?= $varosok->getVarosInfo(intval($_GET['vid']))['mnev'] ?></li>
+        <li>Várostípus: <?= $varosok->getVarosInfo(intval($_GET['vid']))['vtip'] ?></li>
+        <li>Járás: <?= $varosok->getVarosInfo(intval($_GET['vid']))['jaras'] ?></li>
+        <li>Kistérség: <?= $varosok->getVarosInfo(intval($_GET['vid']))['kisterseg'] ?></li>
+        <li>Népeség: <?= $varosok->getVarosInfo(intval($_GET['vid']))['nepesseg'] ?> fő</li>
+        <li>Terület: <?= $varosok->getVarosInfo(intval($_GET['vid']))['terulet'] ?> km<sup>2</sup></li>
+    </ul>
 </body>
 </html>
